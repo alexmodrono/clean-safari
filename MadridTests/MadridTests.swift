9b046f4cb4fcb81d63a332c14593e16c9c753ca6
Alejandro Modroño Vara <amodrono@alu.icai.comillas.edu>
2025-11-27 17:04:25 +0100

    Initial Commit

---
+++ MadridTests/MadridTests.swift
@@ -0,0 +1,17 @@
+//
+//  MadridTests.swift
+//  MadridTests
+//
+//  Created by Alejandro Modroño Vara on 27/11/25.
+//
+
+import Testing
+@testable import Madrid
+
+struct MadridTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
